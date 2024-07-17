6e9ecd4ac64014628a681672ba6bfbfbe4cf381b
WINDOWS 11 <fatenbm737@gmail.com>
2024-07-17 19:44:44 +0200

    work2

---
+++ app/src/main/java/com/ilyes/phone/app/src/main/java/com/example/phone/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.phone.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFBCBBBE)
+val Pink80 = Color(0xFFF5C8D5)
+
+val Purple40 = Color(0xFFFFC107)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF839DE9)
